11ca38c20e68cf9615496122541757fd62d49f15
mannaja <manman7852078520@gmail.com>
2025-12-23 23:41:34 +0700

    edit assignment2 to assignment3

--- src/assign2_660610840/assign2_660610840/whisper_660610840.py
+++ src/assign2_660610840/assign2_660610840/whisper_660610840.py
@@ -2,7 +2,6 @@
 
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
 
 
@@ -10,30 +9,58 @@ class Whisper_660610840(Node):
 
     def __init__(self):
         super().__init__('whisper_660610840')
-        self.publisher_ = self.create_publisher(String, '/gossip_660610840', 10)
+
+        # ===== Declare parameters with default values =====
+        self.declare_parameter('who', 'mannaja')
+        self.declare_parameter('speaking', 'shoutout')
+        self.declare_parameter('spk_msg', 'Successful assignment')
+
+        # ===== Get parameter values =====
+        self.who = self.get_parameter('who').get_parameter_value().string_value
+        self.speaking = self.get_parameter('speaking').get_parameter_value().string_value
+        self.spk_msg = self.get_parameter('spk_msg').get_parameter_value().string_value
+
+        # ===== Publisher =====
+        self.publisher = self.create_publisher(
+            String,
+            '/gossip_660610840',
+            10
+        )
+
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
+        self.who = self.get_parameter('who').get_parameter_value().string_value
+        self.speaking = self.get_parameter('speaking').get_parameter_value().string_value
+        self.spk_msg = self.get_parameter('spk_msg').get_parameter_value().string_value
+        
         msg = String()
-        msg.data = 'Oh My ROS, I am 660610840: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('PUB whisper: "%s"' % msg.data)
+
+        if (self.who == 'mannaja' and self.speaking == 'shoutout' and self.spk_msg == 'Successful assignment'):
+            self.spk_msg = 'Oh My ROS, I am 660610840'
+            self.get_logger().info(f'PUB whisper: {self.spk_msg}: {self.i}')
+        
+        else:                
+            self.get_logger().info(
+                f'{self.who} {self.speaking} {self.spk_msg}: {self.i}'
+            )
+
+        msg.data = f'{self.spk_msg}: {self.i}'
+
+        self.publisher.publish(msg)
+
         self.i += 2
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    whisper_660610840 = Whisper_660610840()
+    node = Whisper_660610840()
+    rclpy.spin(node)
 
-    rclpy.spin(whisper_660610840)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    whisper_660610840.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 
